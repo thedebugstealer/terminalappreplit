f2872d0663c2611985215f851f8f526d65be2e4a
thedebugstealer <savagekyler030@gmail.com>
2026-03-29 20:14:06 -0700

    new thing: buildozer.py

---
+++ buildozer.py
@@ -0,0 +1,13 @@
+[app]
+title = TerminalApp
+package.name = terminalapp
+package.domain = org.example
+source.dir = .
+source.include_exts = py
+version = 1.0
+requirements = python3,kivy,kivymd
+orientation = portrait
+fullscreen = 0
+
+[buildozer]
+log_level = 2
